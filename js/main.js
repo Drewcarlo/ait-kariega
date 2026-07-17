d1c1a3b4af5d7409314664cf918cda158e98f458
Drewcarlo <148451290+Drewcarlo@users.noreply.github.com>
2026-07-17 10:14:22 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,3 +52,41 @@ counters.forEach(counter => {
     counterObserver.observe(counter);
 
 });
+
+const form = document.getElementById("whatsappForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    const phone = document.getElementById("phone").value;
+
+    const course = document.getElementById("course").value;
+
+    const message = document.getElementById("message").value;
+
+    const whatsappMessage =
+
+`Hello AIT Kariega,
+
+My name is ${name}.
+
+I am interested in the ${course} programme.
+
+Phone Number:
+${phone}
+
+My enquiry:
+
+${message}
+
+I look forward to hearing from you.`;
+
+    const url =
+`https://wa.me/27726096649text=${encodeURIComponent(whatsappMessage)}`;
+
+    window.open(url, "_blank");
+
+});
